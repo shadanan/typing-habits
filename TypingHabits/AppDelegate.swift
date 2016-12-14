1ec305c1d65e9974cfcd045b6bcdb951ae06fabf
Shad Sharma <shadanas@amazon.com>
2016-12-13 19:52:52 -0800

    initial commit

---
+++ TypingHabits/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  TypingHabits
+//
+//  Created by Shad Sharma on 12/13/16.
+//  Copyright © 2016 Shad Sharma. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
